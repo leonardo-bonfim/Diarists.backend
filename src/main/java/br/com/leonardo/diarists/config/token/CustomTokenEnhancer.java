8ebec34dfa74c17de24b33cab1ab807b685d70aa
Leonardo Luiz Bonfim Siqueira <leonardolbs.bonfim@gmail.com>
2020-03-15 10:28:25 -0300

    Adicionado Endereco ao Token Jwt

--- src/main/java/br/com/leonardo/diarists/config/token/CustomTokenEnhancer.java
+++ src/main/java/br/com/leonardo/diarists/config/token/CustomTokenEnhancer.java
@@ -18,6 +18,7 @@ public class CustomTokenEnhancer implements TokenEnhancer {
 		
 		Map<String, Object> addInfo = new HashMap<>();
 		addInfo.put("nome", usuarioSistema.getUsuario().getNome());
+		addInfo.put("endereco", usuarioSistema.getUsuario().getEndereco());
 		
 		((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(addInfo);
 		return accessToken;
